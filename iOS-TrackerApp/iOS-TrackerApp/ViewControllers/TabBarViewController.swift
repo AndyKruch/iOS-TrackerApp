660c44748419bd1e2b7916c4d6029d912ad64a14
Andy Kruch <avk.16@yandex.ru>
2023-07-31 15:59:51 +0300

    tabbarviewController added

---
+++ iOS-TrackerApp/iOS-TrackerApp/ViewControllers/TabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarViewController.swift
+//  iOS-TrackerApp
+//
+//  Created by Andy Kruch on 31.07.23.
+//
+
+import Foundation
+
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
