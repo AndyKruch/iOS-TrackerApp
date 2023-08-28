0b9bd5d8d05d4dc69e841892fba96bfde2a425f7
Andy Kruch <avk.16@yandex.ru>
2023-08-28 17:42:54 +0300

    Added: -TrackerCategoryStore -TrackerRecordStore -TrackerStore
    -persistentContainer in AppDelegate
    
    CoreData model renamed

---
+++ iOS-TrackerApp/CoreData/TrackerRecordStore.swift
@@ -0,0 +1,8 @@
+//
+//  TrackerRecordStore.swift
+//  iOS-TrackerApp
+//
+//  Created by Andy Kruch on 28.08.23.
+//
+
+import Foundation
---
+++ iOS-TrackerApp/CoreData/TrackerStore.swift
@@ -0,0 +1,8 @@
+//
+//  TrackerStore.swift
+//  iOS-TrackerApp
+//
+//  Created by Andy Kruch on 28.08.23.
+//
+
+import Foundation
